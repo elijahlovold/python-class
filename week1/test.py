a4691f94f1e75f6f93088df0a7e90b6fb356d53e
elijah_lovold <elovold@cadenceneuro.com>
2024-11-15 20:59:13 -0800

    week1

---
+++ week1/test.py
@@ -0,0 +1,72 @@
+    # * int 
+    # * float
+    
+    # * str 
+
+# hehlkajsdflkjaslkdfhkjhdblkjfhsda
+
+# = + - * / **
+# & | == != > < <= >=
+
+# << >> ^ 
+
+
+# string1 = "hello" 
+# string2 = " world"
+
+# output = "hello world"
+
+# num1 = 234
+# num2 = 98
+
+# output = ((num1 + num2) * num1 - num2) / num1
+
+# num1 = int(input("input num1"))
+# num2 = int(input("input num2"))
+
+# output = num1 + num2
+# output *= num1 
+# output -= num2 
+# output /= num1 
+
+# print(output)
+
+num1 = int(input("nm1: "))
+num2 = int(input("nm2: "))
+
+operation = input("operation: ")
+
+if (operation == "add") | (operation == "+"): 
+    print(num1 + num2)
+
+if operation == "sub": 
+    print(num1 - num2)
+
+if operation == "mult": 
+    print(num1 * num2)
+
+if operation == "div": 
+    print(num1 / num2)
+
+# # computer x^n
+# result = x**n
+# print(result)
+
+# balance = 34
+# print(balance)
+
+# foo = 20
+# print(balance)
+
+# balance = foo + bar
+# print(balance)
+
+# foo = 323
+# foo = int(323.234)
+
+# foo = True
+# foo = False
+
+# foo = "hello"
+# foo = 'hello'
+
