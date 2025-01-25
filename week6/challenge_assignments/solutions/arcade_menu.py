0d178336d6ea80ab9f2c840452f551eb4d3fcd8b
elovold <elovold@cadenceneuro.com>
2025-01-24 17:56:52 -0800

    fix up some things

---
+++ week6/challenge_assignments/solutions/arcade_menu.py
@@ -0,0 +1,10 @@
+# import game functions from other files...
+
+# Menu to play the games
+while True:
+    print("\nChoose a game:")
+    print("1. Hangman")
+    print("2. Dungeon Escape")
+    print("3. Word Scramble")
+    print("4. Quit")
+
